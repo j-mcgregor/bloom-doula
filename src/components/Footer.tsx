410d9cfd1faf8a07f17b2f3e2ea4078470270b2a
Jack <jmcgregor@coveo.com>
2021-09-16 16:57:13 -0400

    added img links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,8 +6,8 @@ export const Footer = () => {
             <div className="border-t border-b border-gray-200 dark:border-gray-700 py-16">
                 <div className="mx-auto container px-4 xl:px-12 2xl:px-4">
                     <div className="lg:flex">
-                        <div className="w-full flex justify-between">
-                            <div className="w-full lg:w-1/2 px-6">
+                        <div className="w-full flex flex-col-reverse sm:flex-row justify-between">
+                            <div className="w-full lg:w-1/2 px-6 py-4 sm:py-0">
                                 <ul>
                                     <li>
                                         <a
@@ -24,11 +24,32 @@ export const Footer = () => {
                                             </a>
                                         </Link>
                                     </li>
+                                    <li className="mt-6">
+                                        <span className="text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50">
+                                            © 2021 Bloom Doula. All Rights Reserved.
+                                        </span>
+                                    </li>
                                 </ul>
                             </div>
-                            <p className="mt-6 text-xs lg:text-sm leading-none text-gray-900 dark:text-gray-50">
-                                © 2021 Bloom Doula. All Rights Reserved.
-                            </p>
+                            <div className="w-full lg:w-1/2 flex flex-col sm:flex-row-reverse items-center sm:justify-between  py-4 sm:py-0">
+                                <a
+                                    href="https://www.thedouladirectory.com/kings-langley/doulas/laura-mcgregor?from=badge"
+                                    title="Find me on The Doula Directory"
+                                    target="_blank"
+                                >
+                                    <img
+                                        src="https://www.thedouladirectory.com/images/Doula-Directory-Listing-Logo-(2).png"
+                                        className="h-32 sm:h-48"
+                                    />
+                                </a>
+                                <a
+                                    href="https://www.birthblissdoulacourses.co.uk/"
+                                    title="Birth Bliss Courses"
+                                    target="_blank"
+                                >
+                                    <img src="/BirthBlissAcademy.png" className="h-32 sm:h-48" />
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
